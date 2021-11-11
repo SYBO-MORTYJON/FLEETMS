64f4d9ee617f128610ffa540e6158e9226c1f4d4
Shohjahonbek <imomovshoh21@gmail.com>
2021-11-11 21:22:48 +0300

    MVC

--- src/main/java/com/napa/Hospital/parameters/controllers/CountryController.java
+++ src/main/java/com/napa/Hospital/parameters/controllers/CountryController.java
@@ -1,12 +1,26 @@
 package com.napa.Hospital.parameters.controllers;
 
+import com.napa.Hospital.parameters.models.Country;
 import com.napa.Hospital.parameters.services.CountryService;
+import net.bytebuddy.agent.builder.AgentBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class CountryController {
 
     @Autowired
     private CountryService countryService;
+
+    @GetMapping("/countries")
+    public String getAll( Model model) {
+         List<Country> countries= countryService.getAll();
+         model.addAttribute("countries",countries);
+         return "parameters/countryList";
+    }
 }
--- src/main/java/com/napa/Hospital/parameters/services/CountryService.java
+++ src/main/java/com/napa/Hospital/parameters/services/CountryService.java
@@ -1,13 +1,19 @@
 package com.napa.Hospital.parameters.services;
 
+import com.napa.Hospital.parameters.models.Country;
 import com.napa.Hospital.parameters.repositories.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CountryService {
 
     @Autowired
     private CountryRepository countryRepository;
+
+    public List<Country> getAll(){
+        return countryRepository.findAll();}
 
 }
